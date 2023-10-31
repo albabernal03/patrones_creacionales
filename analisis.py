142980fe58b2ac48e1482be52e416662772e63ad
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-01 00:05:35 +0100

    estamos nalisando el csv

--- analisis.py
+++ analisis.py
@@ -3,8 +3,9 @@ import pandas as pd
 URL = "https://datos.madrid.es/egob/catalogo/212504-0-emergencias-activaciones.csv"
 # Leer CSV desde la URL
 
+#leemos el dataset
 data = pd.read_csv(URL, sep=';', encoding='ISO-8859-1')
 
- 
+#Mostramos la información del dataset
+data.info()
 
-print(data.head())  # Mostrar las primeras filas para visualizar los datos
