10cab79236535ba5ffed6589010e0db3be870b62
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-01 00:49:52 +0100

    estamos llevando a cabo un análisis

--- analisis.py
+++ analisis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+mport mathplotlib.pyplot as plt
 
 URL = "https://datos.madrid.es/egob/catalogo/212504-0-emergencias-activaciones.csv"
 # Leer CSV desde la URL
@@ -7,5 +8,25 @@ URL = "https://datos.madrid.es/egob/catalogo/212504-0-emergencias-activaciones.c
 data = pd.read_csv(URL, sep=';', encoding='ISO-8859-1')
 
 #Mostramos la información del dataset
-data.info()
+data.info() #Esto nos sirve para ver el tipo de datos que tenemos en cada columna, y si nos aporta información relevante
 
+##Ahora vamos a eliminar las columnas que no nos sirven para el análisis (columnas que tienen todos los valores nulos (NaN))
+data.drop(['PRECIO', 'DIAS-EXCLUIDOS', 'DESCRIPCION', 'Unnamed: 29'], axis=1, inplace=True)
+print(data)
+
+"""
+Como el dataset que se nos ha proporcionado contiene información 
+sobre El SAMUR-Protección Civil que es es el servicio de atención a 
+urgencias y emergencias sanitarias y se nos pide 
+mostar la media de activaciones por día luego nuestra columna más relevante 
+para ello sería la de FECHA.
+
+"""
+
+#Ahora vamos a hacer una matriz de correlaciones para ver que columnas están más correlacionadas con la columna de FECHA
+corr = data.corr()
+plt.figure(figsize=(10,10))
+sns.heatmap(corr,annot=True,cmap="coolwarm")
+plt.show()
+#Guardamos la imagen
+plt.savefig("../images/correlaciónvariables_precio_.png")
