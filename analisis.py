69e4d8336c3dc61b4400237112bc5a782925132d
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-01 01:53:16 +0100

    estamoos viendo como esta relacionado

--- analisis.py
+++ analisis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import datetime
 
 URL = "https://datos.madrid.es/egob/catalogo/212504-0-emergencias-activaciones.csv"
 # Leer CSV desde la URL
@@ -12,7 +13,7 @@ data = pd.read_csv(URL, sep=';', encoding='ISO-8859-1')
 data.info() #Esto nos sirve para ver el tipo de datos que tenemos en cada columna, y si nos aporta información relevante
 
 ##Ahora vamos a eliminar las columnas que no nos sirven para el análisis (columnas que tienen todos los valores nulos (NaN))
-data.drop(['PRECIO', 'DIAS-EXCLUIDOS', 'DESCRIPCION', 'Unnamed: 29'], axis=1, inplace=True)
+data.drop(['PRECIO', 'DIAS-EXCLUIDOS', 'DESCRIPCION', 'Unnamed: 29','GRATUITO'], axis=1, inplace=True)
 print(data)
 
 """
@@ -24,13 +25,41 @@ para ello sería la de FECHA.
 
 """
 
-#Ahora vamos a hacer una matriz de correlaciones para ver que columnas están más correlacionadas con la columna de FECHA
+# Convertir la columna de fecha en formato datetime
+data['FECHA'] = pd.to_datetime(data['FECHA'])
+# Extraer el día de la semana de la columna "FECHA"
+data['DIA_SEMANA'] = data['FECHA'].dt.dayofweek
+# Crear variables ficticias para el día de la semana y asignar los nombres deseados
+variables_ficticias = pd.get_dummies(data['DIA_SEMANA']).rename(columns={
+    0: 'Lunes',
+    1: 'Martes',
+    2: 'Miércoles',
+    3: 'Jueves',
+    4: 'Viernes',
+    5: 'Sábado',
+    6: 'Domingo'
+})
+
+# Concatenar las variables ficticias al dataframe original
+data = pd.concat([data, variables_ficticias], axis=1)
+
+# Calcular la matriz de correlación
+matriz_correlacion = data.corr()
+
+# Mostrar la matriz de correlación
+print(matriz_correlacion)
+
+# Crear una nueva columna numérica con la fecha convertida
+data['FECHA_NUM'] = data['FECHA'].apply(lambda x: x.toordinal())
+
+# Calcular la matriz de correlaciones
 corr = data.corr()
-plt.figure(figsize=(10,10))
+
+# Visualizar la matriz de correlaciones
+plt.figure(figsize=(10,10)) 
 sns.heatmap(corr,annot=True,cmap="coolwarm")
 plt.show()
-print(data.columns)
 
-
-
+# Guardar la imagen de la matriz de correlaciones
+plt.savefig('matriz_correlaciones.png')
 
