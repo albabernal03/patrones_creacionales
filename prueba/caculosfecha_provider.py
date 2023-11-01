e4902d9f0a8ff1f51891d936b2cf78545294b806
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-01 20:40:39 +0100

    j

--- prueba/caculosfecha_provider.py
+++ prueba/caculosfecha_provider.py
@@ -5,12 +5,11 @@ from calculosfecha_factory import Calculos_estadistico_fecha
 
 class Calculosfechaprovider:
     @staticmethod
-    def create_calculos(elegircal: MMM) --> Calculos_estadisticos_fecha:
-        match elegircal:
-            case MMM.MEDIA:
-                return Mediafecha()
-            case MMM.MODA:
-                return Modafecha()
-            case _:
-                raise ValueError(f'Invalid elegircal: {elegircal}')
+    def create_calculos(elegircal: MMM) -> Calculos_estadisticos_fecha:
+        if elegircal == MMM.MEDIA:
+            return Calculos_estadistico_fecha()
+        elif elegircal == MMM.MODA:
+            return Mediafecha()
+        else:
+            raise ValueError(f'Invalid product {elegircal}')
     
