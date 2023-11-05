7599b3c06bf26857a63ce50cc1445435387cf5e2
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-05 19:25:51 +0100

    definimos en primer lugar el producto abstracto

---
+++ builder_pattern/builder.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+#-----------------------------------------
+#Product
+#-----------------------------------------
+
+class Pizza():
+    def __init__(self):
+        self.masa= ''
+        self.salsa= ''
+        self.ingredientes_principales= []
+        self.coccion= [] 
+        self.presentacion= ''
+        self.maridaje_recomendado= ''
+        self.extra= []
+
+
