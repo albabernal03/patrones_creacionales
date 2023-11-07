dab6c3d27caaca7efe2f30c4305e38d19ab8fd86
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-07 11:17:57 +0100

    borramos cosas que no necesitemos

--- builder_pattern/builder.py
+++ builder_pattern/builder.py
@@ -14,4 +14,43 @@ class Pizza():
         self.maridaje_recomendado= ''
         self.extra= []
 
+#-----------------------------------------
+#Abstract Builder
+#-----------------------------------------
+
+class PizzaBuilder(ABC):
+
+    @abstractmethod
+    def añadir_masa(self):
+        pass
+
+    @abstractmethod
+    def añadir_salsa(self):
+        pass
+
+    @abstractmethod
+    def añadir_ingredientes_principales(self):
+        pass
+
+    @abstractmethod
+    def añadir_coccion(self):
+        pass
+
+    @abstractmethod
+    def añadir_presentacion(self):
+        pass
+
+    @abstractmethod
+    def añadir_maridaje_recomendado(self):
+        pass
+
+    @abstractmethod
+    def añadir_extra(self):
+        pass
+
+    @abstractmethod
+    def get_pizza(self):
+        return self.pizza
+    
+
 
