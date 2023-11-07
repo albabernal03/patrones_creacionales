0d55ab77306ee6f397304d40bf4c95010189c2df
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-07 22:13:10 +0100

    separamdo

---
+++ Abstract_factory/media.py
@@ -0,0 +1,9 @@
+from calculosestadisticos import CalculosEstadisticos
+import pandas as pd
+from numpy import mean
+
+class Media(CalculosEstadisticos):
+    def calcular(self) -> float:
+        datos=pd.read_csv("emergencias.csv")
+        activaciones_por_dia = datos.groupby(datos['FECHA']).size()
+        return (f'La media es: {activaciones_por_dia.mean()}')
