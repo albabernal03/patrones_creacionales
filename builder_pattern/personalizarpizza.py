d3a48a9895aec72a29a7c1bdb19a951db9f34141
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-08 00:55:06 +0100

    cambios

--- builder_pattern/personalizarpizza.py
+++ builder_pattern/personalizarpizza.py
@@ -1,1 +1,76 @@
 from pizza_builder import PizzaBuilder 
+from pizza import Pizza
+
+#-----------------------------------------
+#Concrete Builder
+#-----------------------------------------
+
+class PizzaCustomizadaBuilder(PizzaBuilder):
+
+    def __init__(self):
+        self.pizza = Pizza()
+
+    def añadir_masa(self):
+        self.pizza.masa= input('¿Qué masa quieres? fina o gruesa: ')
+
+    def añadir_salsa(self):
+        self.pizza.salsa= input('¿Qué salsa quieres? tomate o barbacoa: ')
+
+    def añadir_ingredientes_principales(self):
+        opciones_ingredientes = ['tomate', 'queso', 'jamon', 'atun', 'champiñones', 'bacon', 'cebolla', 'pollo', 'piña',
+                                 'aceitunas', 'anchoas', 'maiz', 'salchichas', 'pimiento', 'gambas', 'carne picada', 'huevo']
+        
+        print("Elige los ingredientes principales para tu pizza:")
+        for i, ingrediente in enumerate(opciones_ingredientes, start=1):
+            print(f"{i}. {ingrediente}")
+
+        seleccionados = input("Ingresa los números de los ingredientes separados por comas: ")
+        numeros_seleccionados = [int(x) for x in seleccionados.split(',') if x.isdigit()]
+
+        ingredientes_seleccionados = [opciones_ingredientes[i - 1] for i in numeros_seleccionados]
+
+        self.pizza.ingredientes_principales = ingredientes_seleccionados
+
+    def añadir_coccion(self):
+        self.pizza.coccion= input('¿Cómo quieres que se cocine? al horno o a la piedra: ')
+
+    def añadir_presentacion(self):
+        self.pizza.presentacion= input('¿Cómo quieres que se presente? entera o en porciones: ')
+
+    def añadir_maridaje_recomendado(self):
+        bebidas={
+            'cerveza': ['pollo', 'bacon', 'salchichas', 'carne picada', 'huevo', 'queso'],
+            'vino blanco': ['gambas', 'atun', 'anchoas','queso', 'champiñones'],
+            'vino_tinto': ['queso', 'aceitunas', 'tomate', 'maiz', 'champiñones', 'pimiento'],
+            'sangria': ['pollo', 'bacon', 'queso', 'huevo', 'atun', 'jamon', 'carne picada'],
+            'cocacola': ['huevo', 'queso', 'jamon', 'bacon', 'champiñones', 'pollo', 'pimiento', 'carne picada', 'salchichas'],
+            'fanta_naranja': ['aceitunas', 'atun', 'anchoas', 'maiz', 'gambas'],
+            'fanta_limon': ['pollo', 'bacon', 'queso', 'salchichas', 'huevo', 'atun', 'jamon', 'carne picada', 'champiñones', 'pimiento'],
+            'agua': ['pollo', 'bacon', 'queso', 'salchichas', 'huevo', 'atun', 'jamon', 'carne picada', 'champiñones', 'pimiento']
+
+        }
+
+        ingredientes_pizza = self.pizza.ingredientes_principales
+
+        maridaje_recomendado= None
+        max_coincidencias = 0
+
+        for bebida, ingredientes_bebida in bebidas.items():
+            coincidencias= len(set(ingredientes_pizza).intersection(ingredientes_bebida))
+            if coincidencias > max_coincidencias:
+                max_coincidencias = coincidencias
+                maridaje_recomendado = bebida
+
+        print(f"El maridaje recomendado para tu pizza es: {maridaje_recomendado}")
+        cambiar_maridaje = input('¿Quieres cambiar el maridaje recomendado? si o no: ')
+        if cambiar_maridaje == 'si':
+            self.pizza.maridaje_recomendado = input('¿Qué bebida quieres? cerveza, vino blanco, vino tinto, sangria, cocacola, fanta_naranja, fanta_limon o agua: ')
+        else:
+            self.pizza.maridaje_recomendado = maridaje_recomendado
+
+    def añadir_extra(self):
+        self.pizza.extra= input('¿Quieres bordes rellenos de queso? si o no: ')
+        if self.pizza.extra == 'si':
+            self.pizza.extra = 'bordes rellenos de queso'
+        else:
+            self.pizza.extra = 'sin bordes rellenos de queso'
