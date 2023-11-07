a4ec282f39c43c43525a5927b9064c376c8ad5d7
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-07 22:25:10 +0100

    funciona ole ole

--- Abstract_factory/main.py
+++ Abstract_factory/main.py
@@ -1,2 +1,27 @@
 from calculosmmm import Calculosmmm
 from mostrargraficas import Mostrargraficas
+
+#-----------------------------------------
+#Client
+#-----------------------------------------
+
+
+def main():
+    factory=Calculosmmm()
+
+    calculos=factory.crear_calculos()
+
+    for i in calculos:
+        print(i.calcular())
+
+    factory=Mostrargraficas()
+
+    mostrar=factory.crear_graficas()
+
+    for i in mostrar:
+        i.grafica()
+
+
+if __name__ == "__main__":
+    main()
+   
