05ab39619357381013eaa4880642482878632d35
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-07 12:13:36 +0100

    hemos probado que la estructura principal funciona

--- builder_pattern/builder.py
+++ builder_pattern/builder.py
@@ -48,9 +48,76 @@ class PizzaBuilder(ABC):
     def añadir_extra(self):
         pass
 
-    @abstractmethod
-    def get_pizza(self):
-        return self.pizza
+
+#-----------------------------------------
+#Concrete Builder
+#-----------------------------------------
+
+class PizzaCustomizadaBuilder(PizzaBuilder):
+
+    def __init__(self):
+        self.pizza = Pizza()
+
+    def añadir_masa(self):
+        self.pizza.masa= input('¿Qué masa quieres? fina o gruesa: ')
+
+    def añadir_salsa(self):
+        self.pizza.salsa= input('¿Qué salsa quieres? tomate o barbacoa: ')
+
+    def añadir_ingredientes_principales(self):
+        self.pizza.ingredientes_principales= input('¿Qué ingredientes quieres? añádelos separándolos por comas ').split(',')
+
+    def añadir_coccion(self):
+        self.pizza.coccion= input('¿Cómo quieres que se cocine? al horno o a la piedra: ')
+
+    def añadir_presentacion(self):
+        self.pizza.presentacion= input('¿Cómo quieres que se presente? entera o en porciones: ')
+
+    def añadir_maridaje_recomendado(self):
+        #TODO
+        pass
+
+    def añadir_extra(self):
+        self.pizza.extra= input('¿Qué extras quieres? añádelos separándolos por comas ').split(',')
+
+#-----------------------------------------
+#Director
+#-----------------------------------------
+
+class PizzaDirector():
+    def __init__(self):
+        self.builder = None
+
+    @property
+    def builder(self)-> PizzaBuilder:
+        return self._builder
     
+    @builder.setter #este setter es para que el builder se pueda cambiar en tiempo de ejecución
+    def builder(self, builder: PizzaBuilder):
+        self._builder = builder
 
+    def crear_pizza(self):
+        self.builder.añadir_masa()
+        self.builder.añadir_salsa()
+        self.builder.añadir_ingredientes_principales()
+        self.builder.añadir_coccion()
+        self.builder.añadir_presentacion()
+        self.builder.añadir_maridaje_recomendado()
+        self.builder.añadir_extra()
 
+    def get_pizza(self):
+        return self.builder.pizza
+    
+#-----------------------------------------
+#Client
+#-----------------------------------------
+
+if __name__ == "__main__":
+    
+    director = PizzaDirector()
+    director.builder = PizzaCustomizadaBuilder()
+    director.crear_pizza()
+    pizza = director.get_pizza()
+    print(pizza.__dict__)
+
+    
