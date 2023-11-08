c0e23ae7d302880aacd186899a735c3598039198
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-08 03:37:50 +0100

    borrando

--- Proyectoweb/Proyectoweb/urls.py
+++ Proyectoweb/Proyectoweb/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Proyecto project.
+URL configuration for Proyectoweb project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,11 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Proyecto.views import saludo, fecha
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", saludo),
-    path("saludo/", saludo),
-    path("fecha/", fecha),
 ]
