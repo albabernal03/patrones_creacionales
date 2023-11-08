fba8d555e9744b91bb34e5a7fb8735501c975198
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-08 23:46:20 +0100

    vamos a crear para usuario

--- Proyectoweb/Proyectoweb/urls.py
+++ Proyectoweb/Proyectoweb/urls.py
@@ -21,4 +21,5 @@ from django.contrib import admin
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('Proyectowebapp.urls')),
+    path('autenticacion/', include('autenticacion.urls')),
 ]
