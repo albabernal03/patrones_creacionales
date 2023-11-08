1a0ba833060d5f68284dfb4dd87350490760475a
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-08 04:08:08 +0100

    definimos los html

--- Proyectoweb/Proyectowebapp/views.py
+++ Proyectoweb/Proyectowebapp/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, HttpResponse
 # Create your views here. Aqui se crean las vistas de la app, en este caso de la pizzeria
 
 def home(request):
-    return HttpResponse('Home')
+    return render(request, "Proyectowebapp/home.html")
 
 def pedir(request):
-    return HttpResponse('Pedir')
+    return render(request, "Proyectowebapp/pedir.html")
 
