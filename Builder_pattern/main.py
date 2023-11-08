cce642a8c9fdd4ec0089c8827b31b312f37bda0e
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-08 01:07:27 +0100

    vale ahora sí funciona todo

--- Builder_pattern/main.py
+++ Builder_pattern/main.py
@@ -10,22 +10,22 @@ from crearCVS import PizzaCSV
 #-----------------------------------------
 
 if __name__ == "__main__":
-    
     director = PizzaDirector()
     director.builder = PizzaCustomizadaBuilder()
+    csv_writer = PizzaCSV("pizzas.csv")  # Mueva la creación de la instancia de PizzaCSV fuera del bucle
 
     while True:
         director.crear_pizza()
         pizza = director.get_pizza()
 
-        csv_writer = PizzaCSV("pizzas.csv")
         validator = PizzaValidator(director.builder)
         validator.set_pizza(pizza)
 
         while True:
             if validator.verificar_pizza():
+                # Escribe la fila de datos en el archivo CSV
+                csv_writer.write_pizza_to_csv(pizza)
                 break  # Sal del bucle interno después de confirmar
 
         break  # Sal del bucle principal después de confirmar
-
 
