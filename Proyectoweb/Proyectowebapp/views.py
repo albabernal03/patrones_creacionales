94e09562b6606dcc5c8763f40661843785b4f689
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-08 22:47:02 +0100

    probando

--- Proyectoweb/Proyectowebapp/views.py
+++ Proyectoweb/Proyectowebapp/views.py
@@ -3,6 +3,7 @@ from .form import PizzaBuilderForm
 from .models import Pizza
 from .storage import PizzaCSV
 import csv
+import pandas as pd
 # Create your views here. Aqui se crean las vistas de la app, en este caso de la pizzeria
 
 def home(request):
@@ -28,6 +29,15 @@ def pedir(request):
             maridaje_recomendado=form.cleaned_data['maridaje_recomendado']
             extra=form.cleaned_data['extra_bordes_queso']
 
+        return render(request, "Proyectowebapp/pedido_resumen.html", {
+                'masa': masa,
+                'salsa': salsa,
+                'ingredientes_principales': ingredientes_principales,
+                'coccion': coccion,
+                'presentacion': presentacion,
+                'maridaje_recomendado': maridaje_recomendado,
+                'extra': extra,
+            })
         pizza_order= Pizza(
             masa=masa,
             salsa=salsa,
@@ -42,7 +52,7 @@ def pedir(request):
         csv_file_name = 'pizza.csv'
         pizza_csv = PizzaCSV(csv_file_name)
         pizza_csv.write_pizza_to_csv(pizza_order)
-        return HttpResponse("¡Pedido de pizza realizado con éxito!")
+        return render(request,'Proyectowebapp/home.html')
       
     
     else:
@@ -50,18 +60,17 @@ def pedir(request):
 
     return render(request, "Proyectowebapp/pedir.html", {'form': form})
 
+
+
 def ver_csv(request):
-    csv_file_name = 'pizza.csv'
-    pizza_csv = PizzaCSV(csv_file_name)
-    
-    try:
-        with open(pizza_csv.file_path, newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            pizza_data = list(reader)
-    except FileNotFoundError:
-        pizza_data = []  # Manejar si el archivo no existe
-    
-    return render(request, 'Proyectowebapp/ver_csv.html', {'pizza_data': pizza_data})
+    csv_file_name = 'pizza.csv'  # Nombre de tu archivo CSV
+    df = pd.read_csv(csv_file_name)
+
+    # Convertir el DataFrame de pandas a una tabla HTML
+    table_html = df.to_html(classes='table table-striped')
+
+    return render(request, 'Proyectowebapp/ver_csv.html', {'table_html': table_html})
+
     
 
 
