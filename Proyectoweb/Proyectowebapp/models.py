88f80b1990bdf2797b347b713f42569e07696332
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-08 14:57:26 +0100

    estamos pasando a crear el formulario

--- Proyectoweb/Proyectowebapp/models.py
+++ Proyectoweb/Proyectowebapp/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from abc import ABC, abstractmethod
 
 # Create your models here.
+
+#-----------------------------------------
+#Abstract Builder
+#-----------------------------------------
+
+class PizzaBuilder(ABC):
+
+    @abstractmethod
+    def añadir_masa(self):
+        pass
+
+    @abstractmethod
+    def añadir_salsa(self):
+        pass
+
+    @abstractmethod
+    def añadir_ingredientes_principales(self):
+        pass
+
+    @abstractmethod
+    def añadir_coccion(self):
+        pass
+
+    @abstractmethod
+    def añadir_presentacion(self):
+        pass
+
+    @abstractmethod
+    def añadir_maridaje_recomendado(self):
+        pass
+
+    @abstractmethod
+    def añadir_extra(self):
+        pass
+
+#-----------------------------------------
+#Concrete Builder
+#-----------------------------------------
+
+class Pizza():
+    def __init__(self,masa,salsa,ingredientes_principales,coccion,presentacion,maridaje,extra):
+        self.masa= masa
+        self.salsa= salsa
+        self.ingredientes_principales= ingredientes_principales
+        self.coccion= coccion
+        self.presentacion= presentacion
+        self.maridaje_recomendado= maridaje
+        self.extra= extra
