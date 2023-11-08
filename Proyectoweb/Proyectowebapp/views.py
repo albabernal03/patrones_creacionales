79a7df69270798ef354921604d340ee37c203c49
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-08 15:18:30 +0100

    PROBANDO

--- Proyectoweb/Proyectowebapp/views.py
+++ Proyectoweb/Proyectowebapp/views.py
@@ -6,5 +6,18 @@ def home(request):
     return render(request, "Proyectowebapp/home.html")
 
 def pedir(request):
+    if request.method == 'POST':
+        form = PizzaBuilderForm(request.POST)
+        if form.is_valid():
+            masa=form.cleaned_data['masa']
+            salsa=form.cleaned_data['salsa']
+            ingredientes_principales=form.cleaned_data['ingredientes_principales']
+            coccion=form.cleaned_data['coccion']
+            presentacion=form.cleaned_data['presentacion']
+            maridaje_recomendado=form.cleaned_data['maridaje_recomendado']
+            extra=form.cleaned_data['extra']
+
+            
+
     return render(request, "Proyectowebapp/pedir.html")
 
