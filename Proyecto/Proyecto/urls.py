f8e060b7523f4d3d2d3535951f87796621d435e4
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-08 02:55:47 +0100

    ya sale

--- Proyecto/Proyecto/urls.py
+++ Proyecto/Proyecto/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Proyecto.views import saludo
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", saludo),
+    path("saludo/", saludo),
 ]
