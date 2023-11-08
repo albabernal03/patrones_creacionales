5249cfe7fa333d4a4022fcc4ad51b3344977bfbb
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-08 03:46:55 +0100

    va funcionando

--- Proyectoweb/Proyectowebapp/views.py
+++ Proyectoweb/Proyectowebapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+# Create your views here. Aqui se crean las vistas de la app, en este caso de la pizzeria
+
+def home(request):
+    return HttpResponse('Home')
+
+def pedir(request):
+    return HttpResponse('Pedir')
+
