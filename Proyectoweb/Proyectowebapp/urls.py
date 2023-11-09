145d9c6f3f8e5b86431ffacbb068c68745940211
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-09 16:52:02 +0100

    ??

--- Proyectoweb/Proyectowebapp/urls.py
+++ Proyectoweb/Proyectowebapp/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("pedir/", views.pedir, name="pedir"),
     path("ver_csv/", views.ver_csv, name="ver_csv"),
+    path('revisar_pedido/', views.revisar_pedido, name='revisar_pedido'),
+    path('confirmar_modificar_pedido/', views.confirmar_modificar_pedido, name='confirmar_modificar_pedido'),
+    path('modificar_pedido/', views.modificar_pedido, name='modificar_pedido'),
+
 ]
