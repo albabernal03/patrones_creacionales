307757cbd3066c1189c351201cb492f263d100a4
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-09 01:37:13 +0100

    acabamos de traducir a español

--- Proyectoweb/autenticacion/views.py
+++ Proyectoweb/autenticacion/views.py
@@ -3,21 +3,23 @@ from django.views.generic import View
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib import messages
+from django.utils.translation import activate
 
 
 # Create your views here.
 
 class VRegistro(View):
     def get(self,request):
+
+        activate('es')
+
         form= UserCreationForm()
-        #Cambiamos las etiquetas de los campos a español
-        form.fields['username'].label='Nombre de usuario'
-        form.fields['password1'].label='Contraseña'
-        form.fields['password2'].label='Confirmar contraseña'
-        
+
         return render(request,'registro/registro.html',{'form':form})
 
     def post(self,request):
+        activate('es')
+        
         form= UserCreationForm(request.POST)
 
         if form.is_valid():
