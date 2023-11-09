89deca3ba78cff155912e5866ad114ccfe19dc81
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-09 01:33:39 +0100

    prueba

--- Proyectoweb/autenticacion/views.py
+++ Proyectoweb/autenticacion/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib import messages
 
 
 # Create your views here.
@@ -9,6 +10,11 @@ from django.contrib.auth import login
 class VRegistro(View):
     def get(self,request):
         form= UserCreationForm()
+        #Cambiamos las etiquetas de los campos a español
+        form.fields['username'].label='Nombre de usuario'
+        form.fields['password1'].label='Contraseña'
+        form.fields['password2'].label='Confirmar contraseña'
+        
         return render(request,'registro/registro.html',{'form':form})
 
     def post(self,request):
@@ -23,5 +29,9 @@ class VRegistro(View):
             return redirect('home')
         
         else:
-            pass
+            for msg in form.error_messages:
+                messages.error(request,form.error_messages[msg])
+            
+            return render(request, 'registro/registro.html', {'form':form})
+            
 
