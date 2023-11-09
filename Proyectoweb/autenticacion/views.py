ae44c3507aa3fafd287579c40040c95f20397564
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-09 02:27:55 +0100

    va funcionando

--- Proyectoweb/autenticacion/views.py
+++ Proyectoweb/autenticacion/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from django.utils.translation import activate
 
@@ -42,4 +42,26 @@ def cerrar_sesion(request):
     logout(request)
     messages.success(request,"Sesión cerrada exitosamente")
     return redirect('home')
+
+def logear(request):
+    activate('es')
+
+    if request.method=='POST':
+        form= AuthenticationForm(request, data=request.POST)
+            
+        if form.is_valid():
+            nombre_usuario=form.cleaned_data.get('username')
+            contraseña=form.cleaned_data.get('password')
+            usuario=authenticate(username=nombre_usuario,password=contraseña)
+            if usuario is not None:
+                login(request,usuario)
+                return redirect('home')
+            else:
+                messages.error(request,"Los datos son incorrectos")
+
+        else:
+            messages.error(request,"Los datos son incorrectos")
+
+    form= AuthenticationForm()
+    return render(request,'login/login.html',{'form':form})
     
