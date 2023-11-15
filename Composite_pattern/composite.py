80e71e0d549d2c254c2ad17597edd0edf5cdc89a
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-15 02:08:00 +0100

    esto lo modificaremos ya mañana

--- Composite_pattern/composite.py
+++ Composite_pattern/composite.py
@@ -30,7 +30,65 @@ class Pizza(ComponentMenu):
     def mostrar(self):
         print(f'Pizza: {self.nombre} - Precio: {self.precio}')
 
-        
+
+#------------------------------------------------------------
+# Leaf
+#------------------------------------------------------------
+
+class Bebida(ComponentMenu):
+
+    def __init__(self, nombre, precio):
+        self.nombre = nombre
+        self.precio = precio
+
+    def mostrar(self):
+        print(f'Bebida: {self.nombre} - Precio: {self.precio}')
+
+#------------------------------------------------------------
+# Leaf
+#------------------------------------------------------------
+
+class Postre(ComponentMenu):
+    
+        def __init__(self, nombre, precio):
+            self.nombre = nombre
+            self.precio = precio
+    
+        def mostrar(self):
+            print(f'Postre: {self.nombre} - Precio: {self.precio}')
+
+#------------------------------------------------------------
+# Composite
+#------------------------------------------------------------
+
+class Combo(ComponentMenu):
+
+    '''
+    La clase Compuesto representa los componentes complejos que pueden tener hijos. Por lo general, 
+    los objetos Compuesto delegan el trabajo real a sus hijos y luego "resumen" el resultado.
+    '''
+
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.elementos = []
+
+    def agregar(self, elemento):
+        self.elementos.append(elemento)
+
+    def eliminar(self, elemento):
+        self.elementos.remove(elemento)
+    
+    def mostrar(self):
+        print(f'Combo: {self.nombre}')
+
+        for elemento in self.elementos:
+            elemento.mostrar()
+
+
+#TODO: terminar el patrón composite
+
+
+
 
 
     
