5af277eb2f50f6d45ef27135c72d1ee448dd4937
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-16 15:49:40 +0100

    n

--- Proyectoweb/carro/carro.py
+++ Proyectoweb/carro/carro.py
@@ -1,51 +1,54 @@
+# Importa la nueva estructura
+from menu.models import Componente
+
 class Carro:
-    def __init__(self,request):
-        self.request=request
-        self.session=request.session
-        carro=self.session.get('carro')
-        
+    def __init__(self, request):
+        self.request = request
+        self.session = request.session
+        carro = self.session.get('carro')
+
         if not carro:
-            carro=self.session['carro']={}
+            carro = self.session['carro'] = {}
         else:
-            self.carro=carro #Si ya existe un carro, lo guardamos en la variable carro es decir qwue si se va el cliente y vuelve, el carro no se borra
+            self.carro = carro
 
-
-
-    def agregar(self,producto):
-        if(str(producto.id) not in self.carro.keys()):
-            self.carro[producto.id]={
-                'producto_id':producto.id,
-                'nombre':producto.nombre,
-                'precio':str(producto.precio),
-                'cantidad':1,
-                'imagen':producto.imagen.url
+    def agregar(self, componente_id):
+        componente = Componente.objects.get(id=componente_id)
+        if str(componente.id) not in self.carro.keys():
+            self.carro[str(componente.id)] = {
+                'componente_id': componente.id,
+                'nombre': componente.nombre,
+                'precio': str(componente.precio),
+                'cantidad': 1,
+                'imagen': componente.imagen.url
             }
         else:
-            for key,value in self.carro.items():
-                if key==str(producto.id):
-                    value['cantidad']=value['cantidad']+1
+            for key, value in self.carro.items():
+                if key == str(componente.id):
+                    value['cantidad'] = value['cantidad'] + 1
                     break
         self.guardar_carro()
-        
-    def guardar_carro(self):
-        self.session['carro']=self.carro
-        self.session.modified=True
 
-    def eliminar(self,producto):
-        producto.id=str(producto.id)
-        if producto.id in self.carro:
-            del self.carro[producto.id]
+    def guardar_carro(self):
+        self.session['carro'] = self.carro
+        self.session.modified = True
+
+    def eliminar(self, componente_id):
+        componente_id = str(componente_id)
+        if componente_id in self.carro:
+            del self.carro[componente_id]
             self.guardar_carro()
 
-    def restar_producto(self,producto):
-        for key,value in self.carro.items():
-            if key==str(producto.id):
-                value['cantidad']=value['cantidad']-1
-                if value['cantidad']<1:
-                    self.eliminar(producto)
+    def restar_producto(self, componente_id):
+        componente_id = str(componente_id)
+        for key, value in self.carro.items():
+            if key == componente_id:
+                value['cantidad'] = value['cantidad'] - 1
+                if value['cantidad'] < 1:
+                    self.eliminar(componente_id)
                 break
         self.guardar_carro()
 
     def limpiar_carro(self):
-        self.session['carro']={}
-        self.session.modified=True
+        self.session['carro'] = {}
+        self.session.modified = True
--- Proyectoweb/menu/models.py
+++ Proyectoweb/menu/models.py
@@ -1,28 +1,48 @@
+# menu/models.py
 from django.db import models
-from .compositepatron import Pizza, Bebida, Postre, Entrante, Combo, ComboPareja
-# Create your models here.
 
 class CategoriaProd(models.Model):
     nombre = models.CharField(max_length=50)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
 
-    class Meta: #esto es para definir el nombre de la tabla en la base de datos
+    class Meta:
         verbose_name = "categoriaProd"
         verbose_name_plural = "categoriasProd"
 
     def __str__(self):
         return self.nombre
-    
-class Producto(models.Model):
-    nombre=models.CharField(max_length=50)
-    categorias=models.ForeignKey(CategoriaProd, on_delete=models.CASCADE)
-    imagen=models.ImageField(upload_to='menu', null=True, blank=True)
-    precio=models.FloatField()
+
+class Componente(models.Model):
+    nombre = models.CharField(max_length=50)
+    categoria = models.ForeignKey(CategoriaProd, on_delete=models.CASCADE)
+    precio = models.FloatField()
+    imagen = models.ImageField(upload_to='menu', null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name = "componente"
+        verbose_name_plural = "componentes"
 
-    class Meta: #esto es para definir el nombre de la tabla en la base de datos
-        verbose_name = "producto"
-        verbose_name_plural = "productos"
+    def mostrar(self):
+        print(f'{self.categoria}: {self.nombre} - Precio: {self.precio}')
+
+class Combo(models.Model):
+    nombre = models.CharField(max_length=50)
+    componentes = models.ManyToManyField(Componente)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "combo"
+        verbose_name_plural = "combos"
+
+    def mostrar(self):
+        print(f'Combo: {self.nombre}')
+        for componente in self.componentes.all():
+            componente.mostrar()
+        print(f'Precio Total del Combo: {self.calcular_precio_total()}')
+
+    def calcular_precio_total(self):
+        return sum(componente.precio for componente in self.componentes.all())
