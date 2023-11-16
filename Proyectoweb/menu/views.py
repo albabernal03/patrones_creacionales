9276f1cdf4c720a9a73ee885503e3c53f09c136f
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-16 16:31:55 +0100

    vamos a modificar ahora el html

--- Proyectoweb/menu/views.py
+++ Proyectoweb/menu/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Combo # hemos importado el modelo Combo para poder usarlo en la vista menu
 
 # Create your views here.
 def menu(request):
-    return render(request, 'menu/menu.html')
+    combos = Combo.objects.all()
+    return render(request, 'menu/menu.html', {'combos':combos})
