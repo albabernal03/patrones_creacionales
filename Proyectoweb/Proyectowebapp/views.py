ba3b8ef7dfd10200b638ea066c8f5cb71112271d
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-18 15:36:30 +0100

    probando

--- Proyectoweb/Proyectowebapp/views.py
+++ Proyectoweb/Proyectowebapp/views.py
@@ -4,6 +4,7 @@ from .models import Pizza
 from .storage import PizzaCSV
 import csv
 import pandas as pd
+from menu.models import Combo
 
 
 # Create your views here. Aqui se crean las vistas de la app, en este caso de la pizzeria
@@ -117,5 +118,5 @@ def confirmar_modificar_pedido(request):
 
 
 def menu(request):
-
-    return render(request, 'menu/menu.html')
+    combos = Combo.objects.all()
+    return render(request, 'menu/menu.html', {'combos':combos})
