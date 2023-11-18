9449950a35a87abe9fa20b84ced2664785fcefce
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-18 17:59:23 +0100

    fallls

--- Proyectoweb/menu/views.py
+++ Proyectoweb/menu/views.py
@@ -1,7 +1,10 @@
+from django.db import transaction
+
 from django.shortcuts import render
 from menu.models import Combo
 
 # Create your views here.
+@transaction.atomic
 def menu(request):
     combos = Combo.objects.all()
     return render(request, 'menu/menu.html', {'combos':combos})
