5bcb4326827c593332282ac5cac9afcc600c7f12
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-18 16:58:08 +0100

    hemos arrelado la estructura

--- Proyectoweb/Proyectoweb/urls.py
+++ Proyectoweb/Proyectoweb/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('Proyectowebapp.urls')),
     path('autenticacion/', include('autenticacion.urls')),
+    path('menu/', include('menu.urls')),
    
 ]
 
