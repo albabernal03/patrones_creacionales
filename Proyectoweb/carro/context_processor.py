63688c48cb8bdcab97f5503986801d5b690ddbc0
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-19 16:03:25 +0100

    ,

--- Proyectoweb/carro/context_processor.py
+++ Proyectoweb/carro/context_processor.py
@@ -1,7 +1,5 @@
+# En tu context_processor.py modificado
 def importe_total_carro(request):
-    total=0
-    if request.user.is_authenticated:
-        for key, value in request.session["carro"].items():
-            total=total+float(value["precio"]*value["cantidad"])
-    return {"importe_total_carro":total}
-
+    carro = request.session.get("carro", {})  # Usa get() para evitar KeyError
+    total = sum(item['precio_total'] for item in carro.values())
+    return {'importe_total_carro': total}
