9e0d3dbca8fac466fe05c5cde22bf5bc537aa691
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-21 18:19:12 +0100

    m

--- Proyectoweb/peticion/models.py
+++ Proyectoweb/peticion/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User=get_user_model
+class Peticion(models.Model):
+
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+    
+    @property
+    def total(self):
+        pass 
+
+
+    class Meta:
+        dt_table='peticion'
+        verbose_name='peticion'
+        verbose_name_plural='peticiones'
+        ordering=['id']
