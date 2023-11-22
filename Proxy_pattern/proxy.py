dbc8f5220fe44d6f33fdddf79ebe9cefb6fd34b2
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-22 20:28:58 +0100

    definiendo

--- Proxy_pattern/proxy.py
+++ Proxy_pattern/proxy.py
@@ -2,5 +2,15 @@ from abc import ABC, abstractmethod
 
 class Component(ABC):
     @abstractmethod
-    def size(self):
+    def tamaño(self):
         pass
+
+class Documento(Component):
+    def __init__(self, nombre, tipo, tamaño):
+        self.__nombre = nombre
+        self.__tipo = tipo
+        self.__tamaño = tamaño
+
+    def tamaño(self):
+        return self.__size
+      
