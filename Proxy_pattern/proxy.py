63848b070e4bc47d81fca110c180603bfb8037ff
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-22 20:41:43 +0100

    creamos la estructura del composite para el ejercicio del SAMUR

--- Proxy_pattern/proxy.py
+++ Proxy_pattern/proxy.py
@@ -1,9 +1,17 @@
 from abc import ABC, abstractmethod
 
+#------------------------------------------------------------
+# Component
+#------------------------------------------------------------
+
 class Component(ABC):
     @abstractmethod
     def tamaño(self):
         pass
+
+#------------------------------------------------------------
+#Leaf
+#------------------------------------------------------------
 
 class Documento(Component):
     def __init__(self, nombre, tipo, tamaño):
@@ -14,3 +22,39 @@ class Documento(Component):
     def tamaño(self):
         return self.__size
       
+#------------------------------------------------------------
+# Composite
+#------------------------------------------------------------
+
+class Carpeta(Component):
+    def __init__(self, nombre, elementos):
+        self.__nombre = nombre
+        self.__elementos = []
+
+    def tamaño(self):
+        total = 0
+        for elemento in self.__elementos:
+            total += elemento.tamaño()
+        return total
+
+    def agregar(self, elemento):
+        self.__elementos.append(elemento)
+
+    def eliminar(self, elemento):
+        self.__elementos.remove(elemento)
+
+
+#------------------------------------------------------------
+#Leaf
+#------------------------------------------------------------
+
+class Enlace(Component):
+    def __init__(self, nombre, destino):
+        self.__nombre = nombre
+        self.__url = destino
+
+    def tamaño(self):
+        return 0
+    
+
+    
