e995d49cc4409fcbe0781e199d3673fa1f5b4dc6
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-23 16:22:28 +0100

    haciendo que se añada los pedidos

--- Proyectoweb/peticion/views.py
+++ Proyectoweb/peticion/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from peticion.models import Peticion, LineaPeticion
+from carro.carro import Carro
+from django.contrib import messages
+
 
 # Create your views here.
+@login_required(login_url='/autenticacion/login')
+def procesar_peticion(requets):
+    peticion=Peticion.objects.created(user=requets.user)
+    carro=Carro(requets)
+    lineas_peticion=list()
+    for key, value in carro.carro.items():
+        lineas_peticion.append(LineaPeticion(
+            combo_id=key,
+            cantidad=value['cantidad'],
+            user=requets.user,
+            peticion=peticion
+
+
+
+        ))
+
+    LineaPeticion.objects.bulk_create(lineas_peticion) #para añaadir varios objetos a la vez
+
+    messages.success(requets, 'Pedido procesado exitosamente')      
