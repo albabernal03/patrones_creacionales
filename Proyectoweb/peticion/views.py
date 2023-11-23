3ca2d2558aac6f20d263b77b6f92462286450df8
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-23 17:36:35 +0100

    probando

--- Proyectoweb/peticion/views.py
+++ Proyectoweb/peticion/views.py
@@ -4,41 +4,53 @@ from peticion.models import Peticion, LineaPeticion
 from carro.carro import Carro
 from django.contrib import messages
 from django.shortcuts import redirect
+from django.db import transaction
 from django.template.loader import render_to_string 
 from django.utils.html import strip_tags
 from django.core.mail import send_mail
 
-# Create your views here.
 @login_required(login_url='/autenticacion/login')
+@transaction.atomic
 def procesar_peticion(request):
-    peticion = Peticion.objects.create(user=request.user)
-    carro = Carro(request)
-    lineas_peticion = []
+    try:
+        # Paso 1: Crear y guardar la instancia de Peticion
+        peticion = Peticion.objects.create(user=request.user)
 
-    for key, value in carro.carro.items():
-        linea_peticion = LineaPeticion(
-            combo_id=key,
-            cantidad=value['cantidad'],
-            user=request.user,
-            peticion=peticion
+        # Paso 2: Crear instancias de LineaPeticion y agregarlas a la lista
+        carro = Carro(request)
+        lineas_peticion = []
+
+        for key, value in carro.carro.items():
+            linea_peticion = LineaPeticion(
+                combo_id=key,
+                cantidad=value['cantidad'],
+                user=request.user,
+                peticion=peticion
+            )
+            lineas_peticion.append(linea_peticion)
+
+        # Paso 3: Usar bulk_create para insertar todas las instancias de LineaPeticion
+        LineaPeticion.objects.bulk_create(lineas_peticion)
+
+        # Paso 4: Guardar la instancia de Peticion después de crear las LineaPeticion
+        peticion.save()
+
+        # Paso 5: Enviar correo electrónico
+        enviar_email(
+            peticion=peticion, 
+            lineas_peticion=lineas_peticion,
+            nombreusuario=request.user.username, 
+            email_usuario=request.user.email
         )
-        lineas_peticion.append(linea_peticion)
 
-    # Utilizar bulk_create para insertar todas las instancias de LineaPeticion
-    LineaPeticion.objects.bulk_create(lineas_peticion)
+        # Paso 6: Mensaje de éxito y redirección
+        messages.success(request, 'Pedido procesado exitosamente') 
+        return redirect('../menu')
 
-    # Guardar la instancia de Peticion después de crear las LineaPeticion
-    peticion.save()
-
-    enviar_email(
-        peticion=peticion, 
-        lineas_peticion=lineas_peticion,
-        nombreusuario=request.user.username, 
-        email_usuario=request.user.email
-    )
-
-    messages.success(request, 'Pedido procesado exitosamente') 
-    return redirect('../menu')     
+    except Exception as e:
+        # Manejo de errores y re-lanzamiento para depuración
+        print(f"Error en procesar_peticion: {e}")
+        raise
 
 def enviar_email(**kwargs):
     asunto = 'Pedido Procesado'
