65be82a4b7ab191e05cffd45b47ba1e22fca2672
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-23 18:00:55 +0100

    jm

--- Proyectoweb/Proyectoweb/settings.py
+++ Proyectoweb/Proyectoweb/settings.py
@@ -87,7 +87,11 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-    }
+    },
+    "nueva_db": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "nueva_db.sqlite3",
+    },
 }
 
 
