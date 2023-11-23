bbff6f844b2c8de2a6205a0885dc23aa08555d9f
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-23 17:44:06 +0100

    n

--- Proyectoweb/peticion/views.py
+++ Proyectoweb/peticion/views.py
@@ -4,64 +4,51 @@ from peticion.models import Peticion, LineaPeticion
 from carro.carro import Carro
 from django.contrib import messages
 from django.shortcuts import redirect
-from django.db import transaction
 from django.template.loader import render_to_string 
 from django.utils.html import strip_tags
 from django.core.mail import send_mail
 
-@login_required(login_url='/autenticacion/login')
-@transaction.atomic
-def procesar_peticion(request):
-    try:
-        # Paso 1: Crear y guardar la instancia de Peticion
-        peticion = Peticion.objects.create(user=request.user)
+# Create your views here.
+@login_required(login_url='/autenticacion/logear')
+def procesar_peticion(requets):
+    peticion=Peticion.objects.created(user=requets.user)
+    carro=Carro(requets)
+    lineas_peticion=list()
+    for key, value in carro.carro.items():
+        lineas_peticion.append(LineaPeticion(
+            combo_id=key,
+            cantidad=value['cantidad'],
+            user=requets.user,
+            peticion=peticion
 
-        # Paso 2: Crear instancias de LineaPeticion y agregarlas a la lista
-        carro = Carro(request)
-        lineas_peticion = []
 
-        for key, value in carro.carro.items():
-            linea_peticion = LineaPeticion(
-                combo_id=key,
-                cantidad=value['cantidad'],
-                user=request.user,
-                peticion=peticion
-            )
-            lineas_peticion.append(linea_peticion)
 
-        # Paso 3: Usar bulk_create para insertar todas las instancias de LineaPeticion
-        LineaPeticion.objects.bulk_create(lineas_peticion)
+        ))
 
-        # Paso 4: Guardar la instancia de Peticion después de crear las LineaPeticion
-        peticion.save()
+    LineaPeticion.objects.bulk_create(lineas_peticion) #para añaadir varios objetos a la vez
 
-        # Paso 5: Enviar correo electrónico
-        enviar_email(
-            peticion=peticion, 
-            lineas_peticion=lineas_peticion,
-            nombreusuario=request.user.username, 
-            email_usuario=request.user.email
+    enviar_email(
+        peticion= peticion, 
+        lineas_peticion=lineas_peticion,
+        nombre_usuario=requets.user.username, 
+        email_usuario=requets.user.email
         )
 
-        # Paso 6: Mensaje de éxito y redirección
-        messages.success(request, 'Pedido procesado exitosamente') 
-        return redirect('../menu')
-
-    except Exception as e:
-        # Manejo de errores y re-lanzamiento para depuración
-        print(f"Error en procesar_peticion: {e}")
-        raise
+    messages.success(requets, 'Pedido procesado exitosamente') 
+    return redirect('../menu')     
 
 def enviar_email(**kwargs):
-    asunto = 'Pedido Procesado'
-    mensaje = render_to_string('peticion/email.html', {
-        'pedido': kwargs.get('peticion'),
-        'lineas_pedido': kwargs.get('lineas_peticion'),
-        'nombreusuario': kwargs.get('nombreusuario'),
+    asunto= 'Pedido Procesado'
+    mensaje= render_to_string('peticion/email.html',{
+        'pedido': kwargs.get['peticion'],
+        'lineas_pedido': kwargs.get['lineas_peticion'],
+        'nombreusuario': kwargs.get['nombreusuario'],
+
     })
 
-    mensaje_texto = strip_tags(mensaje)
-    from_email = 'pizzeriadelizioso@gmail.com'
-    to = [kwargs.get('email_usuario')]
+    mensaje_texto=strip_tags(mensaje)
+    from_email='pizzeriadelizioso@gmail.com'
+    #to=[kwargs.get['email_usuario']]
+    to='albabr08@gmail.com'
 
-    send_mail(asunto, mensaje_texto, from_email, to, html_message=mensaje)
+    send_mail(asunto, mensaje_texto, from_email,[to], html_message=mensaje)
