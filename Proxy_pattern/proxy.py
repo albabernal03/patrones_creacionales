573265713a166c80e79932c74dc85a37e10c4404
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-25 19:34:01 +0100

    hemos creado la estructura del proxy

--- Proxy_pattern/proxy.py
+++ Proxy_pattern/proxy.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
 
 #------------------------------------------------------------
 # Component
@@ -57,4 +58,37 @@ class Enlace(Component):
         return 0
     
 
-    
+
+#------------------------------------------------------------
+# Proxy
+#------------------------------------------------------------
+
+class ComponentProxy(Component):
+    def __init__(self, real_component, access_control=[]):
+        self._real_component = real_component
+        self._access_control = access_control
+        self._access_granted = False
+        self._access_log = []
+
+    def tamaño(self):
+        if self.check_access():
+            return self._real_component.tamaño()
+        else:
+            return 0
+
+    def check_access(self) -> bool:
+        user = input("Introduce el usuario: ")
+        if user in self._access_control:
+            print(f"Proxy: {user} ha accedido.")
+            self._access_granted = True
+        else:
+            print(f"Proxy: {user} no tiene acceso.")
+        return self._access_granted
+
+    def log_access(self) -> None:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        log_entry = f"Se ha accedido: {timestamp}"
+        print(f"Proxy: {log_entry}")
+        self._access_log.append(log_entry)
+
+        
