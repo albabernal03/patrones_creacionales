d1021bcbdfb50ab5b0208deee62beedd857319ab
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-25 21:31:28 +0100

    haciendo mejoras

--- Composite_pattern/composite.py
+++ Composite_pattern/composite.py
@@ -215,131 +215,133 @@ if __name__ == "__main__":
 
 
     # Mostrar combos predefinidos
-    print("\nCombos predefinidos:")
-    combo_1.mostrar()
-    combo_2.mostrar()
-    combo_3.mostrar()
-    combo_4.mostrar()
-    print("\nCombos Pareja predefinidos:")
-    combo_pareja_1.mostrar()
-    combo_pareja_2.mostrar()
+    while True:
+        print("\nCombos predefinidos:")
+        combo_1.mostrar()
+        combo_2.mostrar()
+        combo_3.mostrar()
+        combo_4.mostrar()
+        print("\nCombos Pareja predefinidos:")
+        combo_pareja_1.mostrar()
+        combo_pareja_2.mostrar()
 
-    print("\nOpciones:")
-    print("1. Crear combo personalizado")
-    print("2. Elegir combo predefinido")
-    print('3. Elegir combo pareja predefinido')
-    print("4. Salir")
-    eleccion = solicitar_opcion("Elige una opción (1, 2, 3 o 4): ", [1, 2, 3, 4])
+        print("\nOpciones:")
+        print("1. Crear combo personalizado")
+        print("2. Elegir combo predefinido")
+        print('3. Elegir combo pareja predefinido')
+        print("4. Salir")
+        eleccion = solicitar_opcion("Elige una opción (1, 2, 3 o 4): ", [1, 2, 3, 4])
 
-    if eleccion == 1:
-        # Solicitar al usuario que elija elementos para el combo personalizado
-        print("\nElige los elementos para tu combo personalizado:")
-        eleccion_entrante = solicitar_opcion(
-            "\nOpciones de entrantes:\n1. Ensalada\n2. Patatas\n3. Alitas de Pollo\n4. Nuggets\n5. Nachos\nElige un entrante (1, 2, 3, 4 o 5): ",
-            [1, 2, 3, 4, 5]
-        )
-        eleccion_pizza = solicitar_opcion(
-            "\nOpciones de pizzas:\n1. Margarita\n2. Pepperoni\n3. Vegetariana\n4. Hawaiana\n5. Cuatro Quesos\nElige una pizza (1, 2, 3, 4 o 5): ",
-            [1, 2, 3, 4, 5]
-        )
-        eleccion_bebida = solicitar_opcion(
-            "\nOpciones de bebidas:\n1. Cola\n2. Agua\n3. Fanta de Naranja\n4. Cerveza\n5. Nestea\nElige una bebida (1, 2, 3, 4 o 5): ",
-            [1, 2, 3, 4, 5]
-        )
-        eleccion_postre = solicitar_opcion(
-            "\nOpciones de postres:\n1. Tarta de queso\n2. Helado\n3. Frutas\n4. Natillas\n5. Tarta de la abuela\nElige un postre (1, 2, 3, 4 o 5): ",
-            [1, 2, 3, 4, 5]
-        )
+        if eleccion == 1:
+            # Solicitar al usuario que elija elementos para el combo personalizado
+            print("\nElige los elementos para tu combo personalizado:")
+            eleccion_entrante = solicitar_opcion(
+                "\nOpciones de entrantes:\n1. Ensalada\n2. Patatas\n3. Alitas de Pollo\n4. Nuggets\n5. Nachos\nElige un entrante (1, 2, 3, 4 o 5): ",
+                [1, 2, 3, 4, 5]
+            )
+            eleccion_pizza = solicitar_opcion(
+                "\nOpciones de pizzas:\n1. Margarita\n2. Pepperoni\n3. Vegetariana\n4. Hawaiana\n5. Cuatro Quesos\nElige una pizza (1, 2, 3, 4 o 5): ",
+                [1, 2, 3, 4, 5]
+            )
+            eleccion_bebida = solicitar_opcion(
+                "\nOpciones de bebidas:\n1. Cola\n2. Agua\n3. Fanta de Naranja\n4. Cerveza\n5. Nestea\nElige una bebida (1, 2, 3, 4 o 5): ",
+                [1, 2, 3, 4, 5]
+            )
+            eleccion_postre = solicitar_opcion(
+                "\nOpciones de postres:\n1. Tarta de queso\n2. Helado\n3. Frutas\n4. Natillas\n5. Tarta de la abuela\nElige un postre (1, 2, 3, 4 o 5): ",
+                [1, 2, 3, 4, 5]
+            )
 
-        # Crear el combo personalizado
-        combo_personalizado = Combo("Combo Personalizado")
-        # Agregar elementos al combo personalizado según las elecciones del usuario
-        if eleccion_entrante == 1:
-            combo_personalizado.agregar(entrante_ensalada)
-        elif eleccion_entrante == 2:
-            combo_personalizado.agregar(entrante_patatas)
-        elif eleccion_entrante == 3:
-            combo_personalizado.agregar(entrante_alitas)
-        elif eleccion_entrante == 4:
-            combo_personalizado.agregar(entrante_nuggets)
-        elif eleccion_entrante == 5:
-            combo_personalizado.agregar(entrante_nachos)
+            # Crear el combo personalizado
+            combo_personalizado = Combo("Combo Personalizado")
+            # Agregar elementos al combo personalizado según las elecciones del usuario
+            if eleccion_entrante == 1:
+                combo_personalizado.agregar(entrante_ensalada)
+            elif eleccion_entrante == 2:
+                combo_personalizado.agregar(entrante_patatas)
+            elif eleccion_entrante == 3:
+                combo_personalizado.agregar(entrante_alitas)
+            elif eleccion_entrante == 4:
+                combo_personalizado.agregar(entrante_nuggets)
+            elif eleccion_entrante == 5:
+                combo_personalizado.agregar(entrante_nachos)
 
-        if eleccion_pizza == 1:
-            combo_personalizado.agregar(pizza_margarita)
-        elif eleccion_pizza == 2:
-            combo_personalizado.agregar(pizza_pepperoni)
-        elif eleccion_pizza == 3:
-            combo_personalizado.agregar(pizza_vegetariana)
-        elif eleccion_pizza == 4:
-            combo_personalizado.agregar(pizza_hawaiana)
-        elif eleccion_pizza == 5:
-            combo_personalizado.agregar(pizza_cuatros_quesos)
+            if eleccion_pizza == 1:
+                combo_personalizado.agregar(pizza_margarita)
+            elif eleccion_pizza == 2:
+                combo_personalizado.agregar(pizza_pepperoni)
+            elif eleccion_pizza == 3:
+                combo_personalizado.agregar(pizza_vegetariana)
+            elif eleccion_pizza == 4:
+                combo_personalizado.agregar(pizza_hawaiana)
+            elif eleccion_pizza == 5:
+                combo_personalizado.agregar(pizza_cuatros_quesos)
 
-        if eleccion_bebida == 1:
-            combo_personalizado.agregar(bebida_cola)
-        elif eleccion_bebida == 2:
-            combo_personalizado.agregar(bebida_agua)
-        elif eleccion_bebida == 3:
-            combo_personalizado.agregar(bebida_fanta_de_naranja)
-        elif eleccion_bebida == 4:
-            combo_personalizado.agregar(bebida_cerveza)
-        elif eleccion_bebida == 5:
-            combo_personalizado.agregar(bebida_nestea)
+            if eleccion_bebida == 1:
+                combo_personalizado.agregar(bebida_cola)
+            elif eleccion_bebida == 2:
+                combo_personalizado.agregar(bebida_agua)
+            elif eleccion_bebida == 3:
+                combo_personalizado.agregar(bebida_fanta_de_naranja)
+            elif eleccion_bebida == 4:
+                combo_personalizado.agregar(bebida_cerveza)
+            elif eleccion_bebida == 5:
+                combo_personalizado.agregar(bebida_nestea)
 
-        if eleccion_postre == 1:
-            combo_personalizado.agregar(postre_tarta_de_queso)
-        elif eleccion_postre == 2:
-            combo_personalizado.agregar(postre_helado)
-        elif eleccion_postre == 3:
-            combo_personalizado.agregar(postre_frutas)
-        elif eleccion_postre == 4:
-            combo_personalizado.agregar(postre_natillas)
-        elif eleccion_postre == 5:
-            combo_personalizado.agregar(postre_tarta_de_la_abuela)
+            if eleccion_postre == 1:
+                combo_personalizado.agregar(postre_tarta_de_queso)
+            elif eleccion_postre == 2:
+                combo_personalizado.agregar(postre_helado)
+            elif eleccion_postre == 3:
+                combo_personalizado.agregar(postre_frutas)
+            elif eleccion_postre == 4:
+                combo_personalizado.agregar(postre_natillas)
+            elif eleccion_postre == 5:
+                combo_personalizado.agregar(postre_tarta_de_la_abuela)
 
-        # Mostrar el combo personalizado
-        print("\nTu combo personalizado:")
-        combo_personalizado.mostrar()
+            # Mostrar el combo personalizado
+            print("\nTu combo personalizado:")
+            combo_personalizado.mostrar()
 
-    elif eleccion == 2:
-        # Solicitar al usuario que elija un combo predefinido y mostrarlo
-        opciones_combos_predefinidos = [1, 2, 3]
-        eleccion_combo_predefinido = solicitar_opcion("Elige un combo predefinido (1, 2 o 3): ", opciones_combos_predefinidos)
+        elif eleccion == 2:
+            # Solicitar al usuario que elija un combo predefinido y mostrarlo
+            opciones_combos_predefinidos = [1, 2, 3]
+            eleccion_combo_predefinido = solicitar_opcion("Elige un combo predefinido (1, 2 o 3): ", opciones_combos_predefinidos)
 
-        if eleccion_combo_predefinido == 1:
-            combo_1.mostrar()
-        elif eleccion_combo_predefinido == 2:
-            combo_2.mostrar()
-        elif eleccion_combo_predefinido == 3:
-            combo_3.mostrar()
+            if eleccion_combo_predefinido == 1:
+                combo_1.mostrar()
+            elif eleccion_combo_predefinido == 2:
+                combo_2.mostrar()
+            elif eleccion_combo_predefinido == 3:
+                combo_3.mostrar()
 
-    elif eleccion == 3:
-        combo_pareja_personalizado = ComboPareja("Combo Pareja Personalizado")
+        elif eleccion == 3:
+            combo_pareja_personalizado = ComboPareja("Combo Pareja Personalizado")
 
-        print("\nElige los combos para tu Combo Pareja:")
-        print("1. Combo 1")
-        print("2. Combo 2")
-        eleccion_combo1 = solicitar_opcion("Elige el Combo 1 (1 o 2): ", [1, 2])
-        eleccion_combo2 = solicitar_opcion("Elige el Combo 2 (1 o 2): ", [1, 2])
+            print("\nElige los combos para tu Combo Pareja:")
+            print("1. Combo 1")
+            print("2. Combo 2")
+            eleccion_combo1 = solicitar_opcion("Elige el Combo 1 (1 o 2): ", [1, 2])
+            eleccion_combo2 = solicitar_opcion("Elige el Combo 2 (1 o 2): ", [1, 2])
 
-        if eleccion_combo1 == 1:
-            combo_pareja_personalizado.personalizar(combo_1, None)
-        elif eleccion_combo1 == 2:
-            combo_pareja_personalizado.personalizar(combo_2, None)
+            if eleccion_combo1 == 1:
+                combo_pareja_personalizado.personalizar(combo_1, None)
+            elif eleccion_combo1 == 2:
+                combo_pareja_personalizado.personalizar(combo_2, None)
 
-        if eleccion_combo2 == 1:
-            combo_pareja_personalizado.personalizar(combo_pareja_personalizado.combo1, combo_1)
-        elif eleccion_combo2 == 2:
-            combo_pareja_personalizado.personalizar(combo_pareja_personalizado.combo1, combo_2)
+            if eleccion_combo2 == 1:
+                combo_pareja_personalizado.personalizar(combo_pareja_personalizado.combo1, combo_1)
+            elif eleccion_combo2 == 2:
+                combo_pareja_personalizado.personalizar(combo_pareja_personalizado.combo1, combo_2)
 
-        # Mostrar el Combo Pareja personalizado
-        print("\nTu Combo Pareja personalizado:")
-        combo_pareja_personalizado.mostrar()
+            # Mostrar el Combo Pareja personalizado
+            print("\nTu Combo Pareja personalizado:")
+            combo_pareja_personalizado.mostrar()
 
-    elif eleccion == 4:
-        print("\nGracias por usar nuestro servicio. Hasta pronto.")
-        exit()
-
-
-
+        elif eleccion == 4:
+            print("\nGracias por usar nuestro servicio. Hasta pronto.")
+            break
+         
+        else:
+            print("Opción no válida. Por favor, elige una opción válida.")
+    
