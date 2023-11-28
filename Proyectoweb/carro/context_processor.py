4d4be7c34aa7d9e3d132606bf272a4277dd7bd3f
albabernal03 <ALBABERO@MYUAX.COM>
2023-11-28 22:19:04 +0100

    c

--- Proyectoweb/carro/context_processor.py
+++ Proyectoweb/carro/context_processor.py
@@ -1,12 +1,20 @@
-# context_processor.py
 def importe_total_carro(request):
     total = 0
 
     if request.user.is_authenticated:
-        carro = request.session.get("carro", {})  
+        carro = request.session.get("carro", {})
 
         for key, value in carro.items():
-            total += float(value.get("precio", 0)) * int(value.get("cantidad", 0))
+            precio = float(value.get("precio", 0))
+            cantidad = int(value.get("cantidad", 0))
+            subtotal = precio * cantidad
+
+            # Apply a 5% discount if the subtotal is greater than 40 euros
+            if subtotal > 40:
+                descuento = subtotal * 0.05
+                subtotal -= descuento
+
+            total += subtotal
 
     else:
         total = 'Debe iniciar sesión'
